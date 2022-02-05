8cc00234044dedfc04381076573508190efe602c
jalis55 <jalismahamud2055@gmail.com>
2022-02-05 15:26:44 +0600

    modifed the codes

--- script.py
+++ script.py
@@ -1,18 +1,17 @@
 import vonage
 
-client=vonage.Client(Key="fc9426bb",secret="Goodway98")
-sms=vonage.sms(client)
+client = vonage.Client(key="fc9426bb", secret="Goodway98")
+sms = vonage.Sms(client)
 def send_sms(num):
-	responseData=sms.send_message({
-	"from":"Hello",
-	"to":num,
-	"text":"hello this is test mesage"
-	})
-	if responseData["message"][0]["status"] =="0":
-		print("Message sent successfully")
-	else:
-		 print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
-	
+    responseData = sms.send_message({
+        "from": "Hello",
+        "to": num,
+        "text": "Hello this is a test message",
+    })
+    if  responseData["messages"][0]["status"] == "0":
+        print("Message sent successfully.")
+    else:
+        print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
 
 
 file=open('numbers.txt','r')
